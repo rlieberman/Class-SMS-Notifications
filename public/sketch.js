4e2c41eab9dcfbab965b81ef896d1aa9ffd6902a
Rebecca Lieberman <rebeccalieberman@gmail.com>
2016-04-03 22:48:06 -0400

    updating base url

--- public/sketch.js
+++ public/sketch.js
@@ -9,7 +9,7 @@ var phoneNumbers = {
   "JohnDoe":"+19175558601"
 };
 
-var base_url = 'http://class-notifications.herokuapp.com/';
+var base_url = '';
 
 $(document).ready(function(){
 
@@ -51,7 +51,7 @@ $(document).ready(function(){
 
     //make a post request so we can grab inputs to send to twilio
     $.ajax({
-        "url": base_url + "twilio",
+        "url": "http://class-notifications.herokuapp.com/twilio",
         "method": "POST",
         "data": data
       })
